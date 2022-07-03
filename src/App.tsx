8776540ddd13b12b9d693efa685b200e69e2d5a0
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-07-04 00:39:19 +0300

    add starrating

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Button} from "./components/Button/Button"
 import './App.scss';
 import {ButtonCancel} from "./components/ButtonCancel/Buttoncancel";
 import ButtonFavorites from './components/ButtonFavorites/ButtonFavorites';
+import StarRating from './components/StarRating/StarRating';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Button text='add to cart'/>
      <ButtonCancel/>
      <ButtonFavorites/>
+     <StarRating rating={5}/>
     </div>
   );
 }
---
+++ src/components/StarRating/StarRating.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import StarRatings from "react-star-ratings";
+
+
+
+interface IStarRaiting {
+  rating: number;
+}
+
+const StarRating = ({ rating }: IStarRaiting) => {
+  return (
+    <StarRatings
+      rating={rating}
+      starRatedColor =" #313037"
+      starEmptyColor="#ffffff"
+      numberOfStars={5}
+      name="rating"
+      starDimension="16px"
+      starSpacing="5px"
+    />
+  );
+};
+
+export default StarRating;
