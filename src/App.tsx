8e5579d025045c8e77a688a1010293abc6c30080
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-07-03 21:19:24 +0300

    add buttonfavoritesn

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Button} from "./components/Button/Button"
 import './App.scss';
 import {ButtonCancel} from "./components/ButtonCancel/Buttoncancel";
+import ButtonFavorites from './components/ButtonFavorites/ButtonFavorites';
 
 function App() {
   return (
     <div className="App">
      <Button text='add to cart'/>
      <ButtonCancel/>
+     <ButtonFavorites/>
     </div>
   );
 }
---
+++ src/components/ButtonFavorites/ButtonFavorites.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Icon from "../Icon/Icon"
+
+import "./ButtonFavorites"
+
+export const ButtonFavorites =()=> {
+    return (
+        <Icon id="favorites"/>
+    )
+}
+export default ButtonFavorites;
