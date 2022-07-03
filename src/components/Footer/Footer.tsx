b0e6af4cdb0eb153c9cfce7b7e49e78c8780136b
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-07-04 00:57:40 +0300

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer--padding-24">
+                <p>©2022 Bookstore</p>
+            </div>
+            <p>All rights reserved</p>
+        </footer>
+    )
+}
+
